09b5d17478c6f5b8957dfd2012287a8a5144bd89
aazuo007-commits <aazo007@gmail.com>
2026-07-22 20:50:29 +0530

    1st commit

--- 1/wirecto/wirecto/backend/routes/partners.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import Partner from "../models/Partner.js";
-import { makeCrudRouter } from "./crudFactory.js";
-
-const router = express.Router();
-router.use("/", makeCrudRouter(Partner));
-
-export default router;
--- 1/wirecto/wirecto/backend/routes/projects.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import Project from "../models/Project.js";
-import { makeCrudRouter } from "./crudFactory.js";
-
-const router = express.Router();
-router.use("/", makeCrudRouter(Project));
-
-export default router;
